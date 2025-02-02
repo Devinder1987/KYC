f8b38993d9701a23b829390a74c44af2269a7bc3
Devinder1987 <d.shutwal@gmail.com>
2025-02-02 22:58:19 +0530

    port adjusted

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ def home():
 
 if __name__ == "__main__":
     print("Starting Flask app...")  # Debug print
-    app.run(debug=True)
+    # app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Use Render's provided port
+    app.run(host="0.0.0.0", port=port, debug=True)
